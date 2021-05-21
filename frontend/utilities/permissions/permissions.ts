d4f14aea2ea1aafc80378040e8b5324bdb791418
RachelElysia <rachel.elysia.perkins@gmail.com>
2021-05-21 11:52:59 -0400

    Query permissions added

--- frontend/utilities/permissions/permissions.ts
+++ frontend/utilities/permissions/permissions.ts
@@ -1,38 +1,51 @@
 import { IUser } from "interfaces/user";
 import { IConfig } from "../../interfaces/config";
 
-const isCoreTier = (config: IConfig): boolean => {
+export const isCoreTier = (config: IConfig): boolean => {
   return config.tier === "core";
 };
 
-const isBasicTier = (config: IConfig): boolean => {
+export const isBasicTier = (config: IConfig): boolean => {
   return config.tier === "basic";
 };
 
-const isGlobalAdmin = (user: IUser): boolean => {
+export const isGlobalAdmin = (user: IUser): boolean => {
   return user.global_role === "admin";
 };
 
-const isGlobalMaintainer = (user: IUser): boolean => {
+export const isGlobalMaintainer = (user: IUser): boolean => {
   return user.global_role === "maintainer";
 };
 
-const isGlobalObserver = (user: IUser): boolean => {
+export const isGlobalObserver = (user: IUser): boolean => {
   return user.global_role === "observer";
 };
 
-const isOnGlobalTeam = (user: IUser): boolean => {
+export const isOnGlobalTeam = (user: IUser): boolean => {
   return user.global_role !== null;
 };
 
-const isTeamObserver = (user: IUser, teamId: number) => {
+export const isTeamObserver = (user: IUser, teamId: number): boolean => {
   const userTeamRole = user.teams.find((team) => team.id === teamId)?.role;
   return userTeamRole === "observer";
 };
 
-const isTeamMaintainer = (user: IUser, teamId: number) => {
+export const isTeamMaintainer = (user: IUser, teamId: number): boolean => {
   const userTeamRole = user.teams.find((team) => team.id === teamId)?.role;
   return userTeamRole === "maintainer";
+};
+
+export const isOnlyObserver = (user: IUser): boolean => {
+  if (isGlobalObserver(user)) {
+    return true;
+  }
+
+  // Return false if any role is team maintainer
+  if (!isOnGlobalTeam) {
+    return !user.teams.some((team) => team?.role === "maintainer");
+  }
+
+  return false;
 };
 
 export default {
@@ -44,4 +57,5 @@ export default {
   isOnGlobalTeam,
   isTeamObserver,
   isTeamMaintainer,
+  isOnlyObserver,
 };
